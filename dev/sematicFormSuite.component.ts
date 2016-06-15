690ce982fc3a8dff5c5d91718272de723962af72
Lars Eidam <lars.eidam@googlemail.com>
2016-06-15 17:45:50 +0200

    include bootstrap for angular2

--- dev/sematicFormSuite.component.ts
+++ dev/sematicFormSuite.component.ts
@@ -1,4 +1,5 @@
 import {Component} from 'angular2/core';
+import {bootstrap} from "angular2/platform/browser";
 import {ROUTER_DIRECTIVES} from 'angular2/router';
 import {RouteConfig} from 'angular2/router';
 import {ConfigurationLoader} from './configuration-loader/configuration-loader.component';
